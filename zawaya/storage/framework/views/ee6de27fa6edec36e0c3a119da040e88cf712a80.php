2f51362443e49b455f5618cfd57ac313b0e66d29
aalroas <mr.a.alroas@gmail.com>
2021-05-03 22:31:32 +0300

    update

--- zawaya/storage/framework/views/ee6de27fa6edec36e0c3a119da040e88cf712a80.php
+++
@@ -1,5 +0,0 @@
-<!--=============== scripts  ===============-->
-<script type="text/javascript" src="<?php echo e(asset('frontend/js/jquery.min.js')); ?>"></script>
-<script type="text/javascript" src="<?php echo e(asset('frontend/js/plugins.js')); ?>"></script>
-<script type="text/javascript" src="<?php echo e(asset('frontend/js/core.js')); ?>"></script>
-<script type="text/javascript" src="<?php echo e(asset('frontend/js/scripts.js')); ?>"></script>
